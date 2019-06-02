c5f351afc0add9d83d26671b78d04a8c6e0fcb7e
Administrator <Administrator@LX>
2019-06-02 16:21:27 +0800

    上传父项目

---
+++ p-parent/src/main/java/com/jzlc/pparent/PParentApplication.java
@@ -0,0 +1,13 @@
+package com.jzlc.pparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PParentApplication.class, args);
+	}
+
+}
